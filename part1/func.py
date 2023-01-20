c245f87d55fe901385b1d888b3f56c3f61c566c4
htrq <119476945+htrq@users.noreply.github.com>
2023-01-20 11:53:42 +0300

    added two functions and print of them

---
+++ part1/func.py
@@ -0,0 +1,12 @@
+def f(x):
+    return x * 2
+
+def super_duper_function(x):
+    if x < 10:
+        return "The number is less than 10"
+    else:
+        return "Now it's okay now"
+
+lolvalue = f("lol")
+print(lolvalue)
+print(super_duper_function(354))
