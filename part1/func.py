847098fc539785364a9597bc1dbf211def9442f7
htrq <119476945+htrq@users.noreply.github.com>
2023-02-21 11:03:48 +0300

    litle clarification in return text

--- part1/func.py
+++ part1/func.py
@@ -5,7 +5,7 @@ def super_duper_function(x):
     if x < 10:
         return "The number is less than 10"
     else:
-        return "Now it's okay now"
+        return "The number is equal or bigger than 10"
 
 lolvalue = f("lol")
 print(lolvalue)
