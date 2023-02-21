d85395aeacf35852d6aa78cf9dd374f8e3717401
htrq <119476945+htrq@users.noreply.github.com>
2023-02-21 11:18:29 +0300

    append and len for lists practiced

--- part1/lists.py
+++ part1/lists.py
@@ -3,3 +3,11 @@ print(list)
 list[1] = 0
 print(list)
 print(list[2:]) # Lists can also as strings be indexed
+print(list[:])  # Some way to out whole list
+
+# method that adds new elements to the end of the list append()
+list.append(609)
+print(list)
+
+# built-in function len as for strings is also appied to lists
+print(len(list))
