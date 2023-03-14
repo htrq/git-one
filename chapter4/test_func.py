5ba2108522d7928bd803bac0e9178e0213ef698c
htrq <119476945+htrq@users.noreply.github.com>
2023-03-14 20:57:42 +0300

    changed way of output

--- chapter4/test_func.py
+++ chapter4/test_func.py
@@ -2,9 +2,9 @@
 i = -5
 
 while(i):
-    print("i variable is: ")
-    print(i)
-    print("incrementing..\n")
+    # this way u can output int and str and etc.
+    print("i variable is: ", i, "incrementing..\n")
     i += 1
 
 print("Now while loop stoped")
+
