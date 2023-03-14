54e4570cff94a42d8c97d4f268260ae9e07c79ca
htrq <119476945+htrq@users.noreply.github.com>
2023-03-14 20:46:59 +0300

    example for while loop

---
+++ chapter4/test_func.py
@@ -0,0 +1,10 @@
+# below example shows that until i var is negative loop continues 
+i = -5
+
+while(i):
+    print("i variable is: ")
+    print(i)
+    print("incrementing..\n")
+    i += 1
+
+print("Now while loop stoped")
