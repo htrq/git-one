0ba6f79d02cd204df0205fd31ccb9a3de47eeae3
htrq <119476945+htrq@users.noreply.github.com>
2023-03-15 14:46:54 +0300

    different variations of range function

--- chapter4/test_for.py
+++ chapter4/test_for.py
@@ -5,5 +5,17 @@ for i in array:
     print(i)
 
 # testing built-in function range() that generates arithmetic progression
+print("\nfirst for:")
 for i in range(5):
+    print(i)
+
+# below futhermore examples of range func
+# this example lets to suggest step over every iteration
+print("\nsecond for:")
+for i in range(0, 10, 2):
+    print(i)
+
+# or in this example we just set range
+print("\nthird for:")
+for i in range(10, 20):
     print(i)
