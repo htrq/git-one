a37bb671ccf1d246e4e42afd7b781aea147daf24
htrq <119476945+htrq@users.noreply.github.com>
2023-03-16 06:24:36 +0300

    pass statement pair of examples

---
+++ chapter4/break_cont.py
@@ -0,0 +1,8 @@
+# pass does nothing it used when program does nothing but sintactically any statement needed
+# example 1 below:
+while(true):
+    pass
+
+# example 2 below:
+class noActionClass:
+    pass
