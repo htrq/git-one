bd3249c3abc6fd84c4909b10c6a4565b975d99d8
htrq <119476945+htrq@users.noreply.github.com>
2023-03-16 06:38:35 +0300

    renamed file correctly

--- chapter4/pass.py
+++ chapter4/pass.py
@@ -6,3 +6,9 @@ while(true):
 # example 2 below:
 class noActionClass:
     pass
+
+# example 3, when you need abstract function but not to realizing right now
+# so here can be used pass statement for a while as a place-holder
+
+def abstractFunc():
+    pass
