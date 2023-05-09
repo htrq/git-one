6115ae69b06c6e3dcafaf0e24f824405951e4e51
htrq <119476945+htrq@users.noreply.github.com>
2023-05-09 16:34:03 +0500

    added coments

--- lab_Vigner/code.py
+++ lab_Vigner/code.py
@@ -11,30 +11,29 @@ def encrypt():
     position = 0
     result_text = ""
 
-    while (len(key_text) < len(text)):
+    while (len(key_text) < len(text)):  # создание последовательности слов ключа
         key_text += key_text
 
     
     for x in range(iteration):
-        if (x > 0):
+        if (x > 0):     # на второй итерации result_text будет передавать прошлое значение и обнулятся
             text = result_text
             result_text = ""
-        for i in range(len(text)):
+        for i in range(len(text)):      # перебор алфавита открытого текста и определение смещения
             for j in range(len(alphabet)):
                 if (text[i] == alphabet[j]):
                     text_shift = j
                     break
 
-            for z in range(len(alphabet)):
+            for z in range(len(alphabet)):  # перебор алфавита ключа и определение смещения
                 if (key_text[i] == alphabet[z]):
                     key_shift = z
                     break
-            position = text_shift + key_shift
-            while (position >= len(alphabet)):
+            position = text_shift + key_shift   # определение финального смещения в алфавите для шифруемой буквы
+            while (position >= len(alphabet)):  # чтобы смещение не зашло за лимит элементов алфавита
                 position -= len(alphabet)
 
-            #print(alphabet[position], end='')
-            result_text += alphabet[position]
+            result_text += alphabet[position]   # запись зашифрованного символа, результат
     print(result_text)
 
 def decrypt():
@@ -47,31 +46,31 @@ def decrypt():
     position = 0
     result_text = ""
 
-    while (len(key_text) < len(text)):
+    while (len(key_text) < len(text)):  # создание последовательности слов ключа
         key_text += key_text
 
     for x in range(iteration):
         if (x > 0):
             text = result_text
             result_text = ""
-        for i in range(len(text)):
+        for i in range(len(text)):      # перебор алфавита зашифрованного текста и определение смещения
             for j in range(len(alphabet)):
                 if (text[i] == alphabet[j]):
                     text_shift = j
                     break
 
-            for z in range(len(alphabet)):
+            for z in range(len(alphabet)):  # перебор алфавита ключа и определение смещения
                 if (key_text[i] == alphabet[z]):
                     key_shift = z
                     break
-            position = text_shift - key_shift
-            while (position < -len(alphabet)):
+            position = text_shift - key_shift   # определение финального смещения в алфавите для расшифровки буквы
+            while (position < -len(alphabet)):  # чтобы смещение не зашло за лимит элементов алфавита
                 position += len(alphabet)
-            result_text += alphabet[position]
+            result_text += alphabet[position]   # запись зашифрованного символа, результат
     print(result_text)
 
 
-task = input("Какую бы опрерацию вы бы хотели выполнить? 1.Шифрование 2.Дешифрование: ")
+task = input("Какую бы опрерацию вы бы хотели выполнить? 1.Шифрование 2.Дешифрование: ")    # меню для пользователя
 if (task == "1"):
     encrypt()
 elif(task == "2"):
